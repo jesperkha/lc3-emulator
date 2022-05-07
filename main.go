11e00df3e102c584d8f2ea63bf474bff5365bda8
jesperkha <hammerjesp@gmail.com>
2022-05-07 23:56:37 +0200

    removed scaffold

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
-import "github.com/jesperkha/smore/vm"
-
 // https://justinmeiners.github.io/lc3-vm/supplies/lc3-isa.pdf
 
 func main() {
-	m := vm.NewMachine()
-	m.ExecuteInstruction()
 }
