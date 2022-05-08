3310d7cafc072c1d565d9151cde509482e730b65
jesperkha <hammerjesp@gmail.com>
2022-05-08 12:09:14 +0200

    AND, BR, and JMP instructions

--- vm/vm.go
+++ vm/vm.go
@@ -33,6 +33,7 @@ func NewMachine() VirtualMachine {
 }
 
 // Sets the appropriate flags based on the result value v
+// Todo: unset flags
 func (m *machine) setFlags(v uint16) {
 	if (v & 0x0001) == 0 {
 		m.registers[reg_FLAG] |= fl_PTY
@@ -45,6 +46,11 @@ func (m *machine) setFlags(v uint16) {
 	}
 }
 
+// Returns true if the flag is set
+func (m *machine) hasFlag(v uint16) bool {
+	return m.registers[reg_FLAG]&v != 0
+}
+
 // Fetches the next instruction from memory and executes it
 func (m *machine) executeInstruction() error {
 	// Fetch instruction and increment pc
@@ -55,12 +61,12 @@ func (m *machine) executeInstruction() error {
 	opcode := bitSequence(ins, 12, 4)
 	regA := bitSequence(ins, 9, 3) // destination register (mostly)
 	regB := bitSequence(ins, 6, 3) // source/base register (mostly)
-	// pcOffset := signExtend(bitSequence(ins, 0, 9), 9)
+	pcOffset := signExtend(bitSequence(ins, 0, 9), 9)
 
 	switch opcode {
-	case op_ADD:
-		addType := bitAt(ins, 6) // bit 6 is 0 for register addition, 1 for immediate value
-		result := uint16(0)      // keep result for flag check after
+	case op_ADD: // ADD <dest>, <source1> <source2|immediate>
+		addType := bitAt(ins, 6) // 0 for register addition, 1 for immediate value
+		result := uint16(0)
 		if addType == 0 {
 			regC := bitSequence(ins, 0, 3) // second source register
 			result = m.registers[regB] + m.registers[regC]
@@ -70,6 +76,32 @@ func (m *machine) executeInstruction() error {
 		}
 		m.registers[regA] = result
 		m.setFlags(result)
+
+	case op_AND: // AND <dest>, <source1> <source2|immediate>
+		andType := bitAt(ins, 6) // 0 for register AND, 1 for immediate value
+		result := uint16(0)
+		if andType == 0 {
+			regC := bitSequence(ins, 0, 3) // second source register
+			result = m.registers[regB] & m.registers[regC]
+		} else {
+			immediate := signExtend(bitSequence(ins, 0, 5), 5)
+			result = m.registers[regB] & immediate
+		}
+		m.registers[regA] = result
+		m.setFlags(result)
+
+	case op_BR: // BR[flags] <label>
+		p, z, n := bitAt(ins, 10) > 0, bitAt(ins, 11) > 0, bitAt(ins, 12) > 0 // flags
+		if m.hasFlag(fl_POS) && p || m.hasFlag(fl_ZRO) && z || m.hasFlag(fl_NEG) && n {
+			m.registers[reg_PC] += pcOffset
+		}
+
+	case op_JMP: // JMP <source> or RET
+		offsetReg := regB
+		if regB == 7 { // source2 is 111 for return
+			offsetReg = reg_R7 // R7 holds return instruction after subroutine call
+		}
+		m.registers[reg_PC] = m.registers[offsetReg]
 
 	default:
 		return errorf(errUnknownOpcode, opcode)
--- vm/vm_test.go
+++ vm/vm_test.go
@@ -1,6 +1,8 @@
 package vm
 
-import "testing"
+import (
+	"testing"
+)
 
 type TestOpcode struct {
 	name         string
@@ -24,11 +26,40 @@ func TestOperations(t *testing.T) {
 			reg_R0,
 			4,
 		},
+		{
+			"AND",
+			[]uint16{
+				0b0001000000101101, // ADD R0, R0 0b10101
+				0b0101001000111111, // AND R0, R0 0b11111
+			},
+			reg_R0,
+			0x000D,
+		},
+		{
+			"BR",
+			[]uint16{
+				0b0001000000000000, // ADD R0, R0 R0
+				0b0000010000000101, // BRz 5
+			},
+			reg_PC,
+			7,
+		},
+		{
+			"JMP",
+			[]uint16{
+				0b0001000000101000, // ADD R0, R0 8
+				0b1100000000000000, // JMP R0
+			},
+			reg_PC,
+			8,
+		},
 	}
 
 	for _, c := range testCases {
 		m.registers[reg_PC] = 0
-		copy(m.memory, c.instructions)
+		copy(m.memory, c.instructions)           // copy instructions into memory
+		copy(m.registers[:], make([]uint16, 16)) // clear registers
+
 		for i := 0; i < len(c.instructions); i++ {
 			if err := m.executeInstruction(); err != nil {
 				t.Errorf("operation %s failed at instruction %d, err %s", c.name, i+1, err)
